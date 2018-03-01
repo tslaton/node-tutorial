823b9190fe40fc9a8c09a24ed2f72eec404879ad
Trevor Slaton <trevor.slaton@gmail.com>
2018-03-01 11:34:14 -0800

    lesson 33: Init Todo app with controller, view, and provided styles

---
+++ app.js
@@ -0,0 +1,19 @@
+// Third-Party
+const express = require('express')
+const bodyParser = require('body-parser')
+// Modules
+const todoController = require('./controllers/todoController')
+
+const app = express()
+// Template engine
+app.set('view engine', 'ejs')
+// Static files
+app.use(express.static('./public'))
+// Body parsing
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+// Fire controllers
+todoController(app)
+
+// Listen to port
+app.listen(3000)
+console.log('Node server is listening on port 3000...')

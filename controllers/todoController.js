d9e735c369efee15f30806f73eecb50b4f887bcc
Trevor Slaton <trevor.slaton@gmail.com>
2018-03-01 11:58:20 -0800

    lesson 34: Hook up placeholder todos

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,14 +1,29 @@
+// Placeholder data
+let data = [
+  {item: 'practice Node.js'},
+  {item: 'enable Babel and ES6 in Node'},
+  {item: 'learn Postgres'},
+  {item: 'learn Hapi?'},
+  {item: 'build app with React and Flask or Node/Hapi'},
+  {item: 'apply to job'}
+]
+
+const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 module.exports = (app) => {
 
   app.get('/todo', (req, res) => {
-    res.render('todo')
+    res.render('todo', { todos: data })
   })
 
-  app.post('/todo', (req, res) => {
-
+  app.post('/todo', urlencodedParser, (req, res) => {
+    data.push(req.body)
+    res.json(data)
   })
 
-  app.delete('/todo', (req, res) => {
-
+  app.delete('/todo/:item', (req, res) => {
+    data = data.filter((todo) => todo.item.replace(/ /g, '-') !== req.params.item)
+    res.json(data)
   })
 }
